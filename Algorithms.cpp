f9a2315e1fed58c3d44cf9dae1b634a0c3d67cc3
Tamjid Hossain <32780359+GunnersFiring@users.noreply.github.com>
2023-01-30 16:35:41 -0500

    Commti

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -14,35 +14,4 @@ int main() {
             x = 0;
         }
     }
-<<<<<<< HEAD
-
-    void algo4 {
-        if (x != 0) {
-            no
-        }
-    }
-
-    void algo5 {
-        //Added new Algo5
-    }
-
-    void Algo6 {
-        // Algo6
-    }
-
-    void Algo7 {
-        // Added in Algo7
-    }
-<<<<<<< HEAD
-
-    void Algo9 {
-        // Added Algo9
-=======
-    
-    void Algo8 {
-        // Added Algo8
->>>>>>> ed30ad0db9082abcf4e75bf77b6d744dc3876382
-    }
-=======
->>>>>>> 29bc08f453cafa993ce1fb8b331ba491fae71bee
 }
