ea838d5add475369b4d64c781731491b89b210a5
Tamjid Hossain <32780359+GunnersFiring@users.noreply.github.com>
2023-01-30 16:43:49 -0500

    Update Algorithms.cpp

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -2,16 +2,14 @@ int main() {
     void algo1 {
         ...
     }
-    
-    void algo2 {
-        for (int i = 0; i < x; i++) {
-            ...   
-        }
-    }
 
     void algo3 {
         while (i != 0) {
             x = 0;
         }
+    }
+    
+    void alog4 {
+        // Algo 4 added by DEV 1   
     }
 }
