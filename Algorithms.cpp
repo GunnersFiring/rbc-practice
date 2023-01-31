d907b6fab1a79d4df0da00a26707ca4f2a41f7a9
Tamjid Hossain <32780359+GunnersFiring@users.noreply.github.com>
2023-01-31 10:55:49 -0500

    Commit Merge

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -8,14 +8,11 @@ int main() {
             x = 0;
         }
     }
-<<<<<<< HEAD
 
     void algo5 {
         // Algo 5 added by DEV 2
-=======
     
     void alog4 {
         // Algo 4 added by DEV 1   
->>>>>>> ea838d5add475369b4d64c781731491b89b210a5
     }
 }
